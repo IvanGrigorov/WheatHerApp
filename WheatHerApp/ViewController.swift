698a64e0713187a413f19a275e01cb353f025e19
Student <student@osbgmbp001s-macbook-pro.local>
2016-12-17 21:14:31 +0200

    Initial Commit

---
+++ WheatHerApp/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  WheatHerApp
+//
+//  Created by Student on 12/17/16.
+//  Copyright © 2016 Student. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
