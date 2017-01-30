f731fb9b49aaf7103ab8e656d9014d412bc4f474
Ivan Grigorov <ivangrigorov9@gmail.com>
2017-01-30 14:48:19 +0200

    New Json

---
+++ WheatHerApp/BackgroundManager.swift
@@ -0,0 +1,9 @@
+//
+//  BackgroundManager.swift
+//  WheatHerApp
+//
+//  Created by Student on 1/19/17.
+//  Copyright © 2017 Student. All rights reserved.
+//
+
+import Foundation
---
+++ WheatHerApp/BackrounfManager.swift
@@ -0,0 +1,9 @@
+//
+//  BackrounfManager.swift
+//  WheatHerApp
+//
+//  Created by Student on 1/19/17.
+//  Copyright © 2017 Student. All rights reserved.
+//
+
+import Foundation
